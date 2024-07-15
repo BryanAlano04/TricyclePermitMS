ee5bde4bb0238216fabe3f1d808decb5906b493a
VonMathewSAnLuis <21-73451@g.batstate-u.edu.ph>
2024-07-16 01:54:13 +0800

    Update admin panel: switch to PHP, add new files and directories

---
+++ admin/index.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<?php include('inc/head.php'); ?>
+<body>
+
+<?php include('inc/sidebar.php'); ?>
+
+	<!-- CONTENT -->
+	<section id="content">
+	<?php include('inc/navbar.php'); ?>
+		<!-- MAIN -->
+		<?php include('inc/dashboard.php'); ?>
+		<!-- MAIN -->
+	</section>
+	<!-- CONTENT -->
+	
+
+	<script src="script.js"></script>
+</body>
+</html>
